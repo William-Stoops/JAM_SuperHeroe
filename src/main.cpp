6450ced34e636fa25473034655c0d363585a40f3
Noa <noa.trachez@epitech.eu>
2023-02-10 19:01:11 +0100

    [+] add next position function

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,23 @@
 */
 
 #include "main.hpp"
+#include <SFML/Graphics.hpp>
+#include <cmath>
 
-void display(sf::RenderWindow *window)
+void nextPosition(sf::Vector2f &position, sf::Vector2f to, float speed)
 {
-    sf::RectangleShape shape(sf::Vector2f(100, 100));
-    shape.setFillColor(sf::Color::Red);
-    sf::RectangleShape shape2(sf::Vector2f(100, 100));
+    sf::Vector2f direction = to - position;
+    float distance = std::sqrt(direction.x * direction.x + direction.y * direction.y);
+    sf::Vector2f unitDirection = direction / distance;
 
-    shape2.setPosition(sf::Vector2f(100, 100));
-    shape2.setFillColor(sf::Color::Magenta);
-    window->draw(shape);
-    window->draw(shape2);
+    sf::Vector2f movement = unitDirection * speed;
+    position += movement;
 }
 
 void sfml(void)
 {
     SFML sfml;
+
     while (sfml.window->isOpen())
     {
         sf::Event event{};
@@ -31,7 +32,6 @@ void sfml(void)
                 sfml.window->close();
         }
         sfml.window->clear();
-        display(sfml.window);
         sfml.window->display();
     }
 }
