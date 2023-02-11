eafc03f0a70de6482fb696b464f06c8f87c28b16
Remi Fernandez <remi.fernandez@epitech.eu>
2023-02-11 14:33:01 +0100

    [+]game over screen

--- src/game_over.cpp
+++ src/game_over.cpp
@@ -7,23 +7,19 @@
 #include "main.hpp"
 
 
-void handle_retry_close(SFML sfml, sf::Event event)
+int handle_click(SFML sfml, sf::Event event, const char *name)
 {
     if (event.type == sf::Event::MouseButtonReleased &&
              event.mouseButton.button == sf::Mouse::Left)
     {
         sf::Vector2i mousePos = sf::Mouse::getPosition(*sfml.window);
-        if (sfml._sprite["retry"].getGlobalBounds().contains(mousePos.x,
-                                                             mousePos.y))
+        if (sfml._sprite[name].getGlobalBounds().contains(mousePos.x,
+                                                          mousePos.y))
         {
-            printf("retry");
-        }
-        if (sfml._sprite["close"].getGlobalBounds().contains(mousePos.x,
-                                                             mousePos.y))
-        {
-            sfml.window->close();
+            return 1;
         }
     }
+    return 0;
 }
 
 void game_over_loop(SFML sfml)
@@ -37,9 +33,19 @@ void game_over_loop(SFML sfml)
                 sfml.window->close();
         }
         sfml.window->clear();
+        float scaleX = (float)sfml.window->getSize().x / sfml
+            ._sprite["retry"].getTexture()->getSize().x;
+        float scaleY = (float)sfml.window->getSize().y / sfml
+            ._sprite["retry"].getTexture()->getSize().y;
+        float scale = std::min(scaleX, scaleY);
+        sfml._sprite["retry"].setScale(scale * 1, scale * 0.9);
+        //sfml._sprite["game_over"].setScale(scale, scale);
+        sfml._sprite["close"].setScale(scale * 1, scale * 0.9);
         sfml.window->draw(sfml._sprite["game_over"]);
         sfml.window->draw(sfml._sprite["retry"]);
         sfml.window->draw(sfml._sprite["close"]);
+        if (handle_click(sfml, event, "close") == 1)
+            sfml.window->close();
         sfml.window->display();
     }
 }
