14725a8bb850fc7a94a088a94d1b149e01dbf9da
Remi Fernandez <remi.fernandez@epitech.eu>
2023-02-11 15:04:02 +0100

    [~]addsprite gameover screen

--- src/SFML/Sfml.cpp
+++ src/SFML/Sfml.cpp
@@ -36,4 +36,17 @@ SFML::SFML() {
     this->rectangle->setOutlineThickness(2);
     this->rectangle->setOutlineColor(sf::Color::White);
     addsprite("spiderman", "./assets/spelunky.png");
+    addsprite("game_over", "./assets/game over.png");
+    addsprite("retry", "./assets/buttons.png");
+    sf::IntRect retry_rect(2036, 208, 200, 200);
+    _sprite["retry"].setScale(0.5,0.5);
+    _sprite["retry"].setPosition(75, 450);
+    _sprite["retry"].setTextureRect(retry_rect);
+    sf::FloatRect retry_bounds = _sprite["retry"].getLocalBounds();
+    _sprite["retry"].setOrigin(retry_bounds.width / 2.f, retry_bounds.height / 2.f);
+    addsprite("close", "./assets/buttons.png");
+    sf::IntRect close_rect(819, 834, 200, 200);
+    _sprite["close"].setScale(0.5,0.5);
+    _sprite["close"].setPosition(625, 450);
+    _sprite["close"].setTextureRect(close_rect);
 }
